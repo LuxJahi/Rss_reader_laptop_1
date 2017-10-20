eadba4e40f88a5aea2cd20a8b8d0c3661dc00ea9
LuxJahi <ojstewart@Hotmail.com>
2017-10-19 22:30:53 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ojste/rss_reader/MainActivity_rss_reader_0.java
@@ -0,0 +1,13 @@
+package com.example.ojste.rss_reader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_rss_reader_0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
